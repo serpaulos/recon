6cbbcae14c1e4879d94def8f7a4ffde476b77b43
Paulo Sergio <43300212+serpaulos@users.noreply.github.com>
2026-01-31 14:42:12 +0000

    Delete src directory

--- src/jira/__init__.py
+++
@@ -1,5 +0,0 @@
-from .jira_client import JiraClient
-from .jira_config import JiraConfig
-from .jira_workflow import create_recon_event_and_attach
-
-__all__ = ["JiraClient", "JiraConfig", "create_recon_event_and_attach"]
--- src/main.py
+++
@@ -1,8 +0,0 @@
-from src.gui.app import ReconApp
-
-def main():
-    app = ReconApp()
-    app.run()
-
-if __name__ == "__main__":
-    main()
